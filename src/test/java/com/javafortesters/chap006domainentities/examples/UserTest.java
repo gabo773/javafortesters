f11d1ca5cc523f35c552b815ff392358149bb66d
Gabriel Morcan <GabrielTeodor.Morcan@endava.com>
2022-08-22 10:34:48 +0300

    Examples from chapter 007

--- src/test/java/com/javafortesters/chap006domainentities/examples/UserTest.java
+++ src/test/java/com/javafortesters/chap006domainentities/examples/UserTest.java
@@ -67,3 +67,4 @@ public class UserTest {
                 user.getPassword());
     }
 }
+;
---
+++ src/test/java/com/javafortesters/chap007basicsofjava/examples/BasicJavaTests.java
@@ -0,0 +1,33 @@
+package com.javafortesters.chap007basicsofjava.examples;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BasicJavaTests {
+
+    @Test
+    public void bitwiseShiftOperatorsExplored(){
+        int x = 56;
+
+        assertEquals(x*2, x<<1);
+        assertEquals(x*4, x<<2);
+        assertEquals(x*8, x<<3);
+        assertEquals(x*16, x<<4);
+        assertEquals(x*32, x<<5);
+
+        x = Integer.MAX_VALUE;
+        assertEquals(Integer.MAX_VALUE/2, x>>1);
+        assertEquals(Integer.MAX_VALUE/4, x>>2);
+        assertEquals(Integer.MAX_VALUE/8, x>>3);
+        assertEquals(Integer.MAX_VALUE/16, x>>4);
+        assertEquals(Integer.MAX_VALUE/32, x>>5);
+    }
+
+    @Test
+    public void someStringMethods(){
+        String aString = "abcdefghij";
+
+        assertEquals("bcd", aString.substring(1,4));
+    }
+}
