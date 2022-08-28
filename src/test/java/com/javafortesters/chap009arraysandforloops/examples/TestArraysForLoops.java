4889d19aed588f1e078735e7e330624e83190977
Gabriel Morcan <GabrielTeodor.Morcan@endava.com>
2022-08-28 14:24:30 +0300

    Chap009 - new methods to test sort arrays

--- src/test/java/com/javafortesters/chap009arraysandforloops/examples/TestArraysForLoops.java
+++ src/test/java/com/javafortesters/chap009arraysandforloops/examples/TestArraysForLoops.java
@@ -3,6 +3,8 @@ package com.javafortesters.chap009arraysandforloops.examples;
 import com.javafortesters.domainentities.User;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class TestArraysForLoops {
@@ -24,9 +26,12 @@ public class TestArraysForLoops {
         users[2] = new User("c_sharp", "pa55w0rd");
 
         assertEquals(3, users.length);
+        assertEquals("gabo", users[0].getUsername());
+        assertEquals("morciu", users[1].getUsername());
+        assertEquals("c_sharp", users[2].getUsername());
 
-       for (User user : users){
-            System.out.println(user);
+       for (User aUser : users){
+            System.out.println(aUser.getUsername());
         }
     }
 
@@ -34,24 +39,71 @@ public class TestArraysForLoops {
     public void oneHundredUsers(){
         User users[] = new User[100];
 
-        int usernameindex = 1;
-        int passwordindex = 1;
-        for (int i = 0; i<users.length; i++){
-            String username = "user" + usernameindex;
-            String password = "password" + passwordindex;
+        for (int i=0; i<users.length;i++){
+            int userID = i+1;
+            String username = "user" + userID;
+            String password = "password" + userID;
             users[i] = new User(username, password);
-            usernameindex++;
-            passwordindex++;
         }
 
         assertEquals("users array contains 100 users"
                 ,100
                 , users.length);
 
-        for(User user : users){
-            assertNotNull("null user in users array", user);
+        int userID = 1;
+        for(User aUser : users){
+            assertEquals("user" + userID
+                    , aUser.getUsername() );
+            assertEquals("password"+userID
+                    , aUser.getPassword());
+            userID++;
+            assertNotNull("null user in users[] array", aUser);
+        }
+    }
+
+    @Test
+    public void sortWorkdays(){
+        String workdays[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+
+        Arrays.sort(workdays);
+        assertEquals("Friday", workdays[0]);
+        assertEquals("Monday", workdays[1]);
+        assertEquals("Thursday", workdays[2]);
+        assertEquals("Tuesday", workdays[3]);
+        assertEquals("Wednesday", workdays[4]);
+    }
+
+    @Test
+    public void sortDifferentCaseWorkdays(){
+        String workdays[] = {"monday", "Tuesday", "Wednesday", "thursday", "Friday"};
+        Arrays.sort(workdays);
+
+        int dayIndex = 0;
+        String sameCaseWorkdays[] = new String[5];
+        for (String day : workdays){
+            sameCaseWorkdays[dayIndex] = capitalizeSingleWords(day);
+            dayIndex++;
         }
 
+        for(String day : sameCaseWorkdays){
+            System.out.println(day);
+        }
+
+        Arrays.sort(sameCaseWorkdays);
+        assertEquals("Friday", sameCaseWorkdays[0]);
+        assertEquals("Monday", sameCaseWorkdays[1]);
+        assertEquals("Thursday", sameCaseWorkdays[2]);
+        assertEquals("Tuesday", sameCaseWorkdays[3]);
+        assertEquals("Wednesday", sameCaseWorkdays[4]);
+    }
+
+    private String capitalizeSingleWords(String word) {
+        word = word.toLowerCase();
+
+        String firstLetter = String.valueOf(word.charAt(0)).toUpperCase();
+        String restOfWord = word.substring(1);
+
+        return firstLetter+restOfWord;
     }
 
 }
